b401236ccf0e1f7920066634304c1c20a37138a4
Michael Angelozzi <mangelozzi@gmail.com>
2023-09-27 17:13:17 +0200

    note about when editing rgflow command

--- .config/nvim/after/plugin/rgflow.lua
+++ .config/nvim/after/plugin/rgflow.lua
@@ -1,11 +1,15 @@
 -- rg --vimgrep --smart-case -g '*.{*,py}' -g '!*.{min.js,pyc}' --fixed-strings --no-fixed-strings --no-ignore -M 500 -g '!**/static/*/jsapp/' -g '!**/static/*/wcapp/' function ~/linkcube/src
+
 require("rgflow").setup(
     {
         cmd_flags = ("--smart-case -g *.{*,py} -g !*.{min.js,pyc} --fixed-strings --no-fixed-strings --no-ignore -M 500"
-            -- distros/cfm/app/meetings/static/meetings/jsapp/fetcher.js
+            -- WARNING when testing changes, dont use "previous search", which will not include these changes, typing in the search term again
+            -- Exclude globs
+            .. " -g !**/.angular/"
             .. " -g !**/node_modules/"
             .. " -g !**/static/*/jsapp/"
-            .. " -g !**/static/*/wcapp/")
+            .. " -g !**/static/*/wcapp/"
+        )
     }
 )
 
