9ae474b792c450f419c10c1156c00b8c0de6ebb4
Michael Angelozzi <mangelozzi@gmail.com>
2023-11-08 11:46:21 +0200

    fix nvim-tree close last buffer

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -339,6 +339,7 @@ vim.api.nvim_set_hl(0, "NvimTreePopup", {fg="#0000ff", bg="#ff0000" })  -- ? - d
 local PluginNvimTreeGroup = vim.api.nvim_create_augroup("PluginNvimTreeGroup", {clear = true})
 vim.api.nvim_create_autocmd(
     "VimEnter" , {
+        group = PluginNvimTreeGroup,
         callback = function (data)
             local is_actual_file = vim.fn.filereadable(data.file) == 1
             if not is_actual_file then
@@ -346,8 +347,31 @@ vim.api.nvim_create_autocmd(
                 require("nvim-tree.api").tree.toggle({focus = false })
             end
         end,
-        group = PluginNvimTreeGroup
-})
+    }
+)
+
+-- Go to last used hidden buffer when deleting a buffer
+-- https://github.com/nvim-tree/nvim-tree.lua/wiki/Recipes#go-to-last-used-hidden-buffer-when-deleting-a-buffer
+vim.api.nvim_create_autocmd(
+    "BufEnter", {
+        group = PluginNvimTreeGroup,
+        nested = true,
+        callback = function()
+            -- Only 1 window with nvim-tree left: we probably closed a file buffer
+            if #vim.api.nvim_list_wins() == 1 and api.tree.is_tree_buf() then
+                -- Required to let the close event complete. An error is thrown without this.
+                vim.defer_fn(function()
+                    -- close nvim-tree: will go to the last hidden buffer used before closing
+                    api.tree.toggle({find_file = true, focus = true})
+                    -- re-open nivm-tree
+                    api.tree.toggle({find_file = true, focus = true})
+                    -- nvim-tree is still the active window. Go to the previous window.
+                    vim.cmd("wincmd p")
+                end, 0)
+            end
+        end
+    }
+)
 
 -- Not used due to slow down
 -- NvimTreeGitDirty xxx guifg=#8cbe17
