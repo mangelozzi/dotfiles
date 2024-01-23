db38bcc92a1b969a30337257e1eef4d41aa138c4
Michael Angelozzi <mangelozzi@gmail.com>
2024-01-23 08:34:10 +0200

    added support for wsl open in chrome

---
+++ .config/nvim/lua/namespace/wsl.lua
@@ -0,0 +1,39 @@
+local M = {}
+
+function M.escape_spaces(path)
+    -- Make the function idempotent
+    local unescaped_path = path:gsub("\\ ", " ") -- Replace '\ ' with ' '
+    local escaped_path = unescaped_path:gsub(" ", "\\ ") -- Escape all spaces
+    return escaped_path
+end
+
+function M.is_wsl()
+    local proc_version_file = io.open("/proc/version", "r")
+    if proc_version_file then
+        local proc_version = proc_version_file:read("*all")
+        proc_version_file:close()
+
+        if proc_version:lower():find("microsoft") then
+            -- The string "microsoft" typically indicates WSL
+            return true
+        else
+            return false
+        end
+    else
+        -- Unable to open /proc/version, assuming not WSL
+        return false
+    end
+end
+
+function M.linux_to_win_path(path)
+    local clean_path = M.escape_spaces(path)
+    local handle = io.popen("wslpath -w " .. clean_path)
+    if not handle then
+        return ''
+    end
+    local win_path = handle:read("*a")
+    local escaped_win_path = win_path:gsub('\\', '\\\\')
+    return escaped_win_path
+end
+
+return M
