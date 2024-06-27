1e29ecdd06f656cc84c5ea51723650dd55e21cfb
Michael Angelozzi <mangelozzi@gmail.com>
2024-06-27 15:51:34 +0200

    hotkeys for various themes

--- .config/nvim/lua/namespace/colors.lua
+++ .config/nvim/lua/namespace/colors.lua
@@ -1,1 +1,5 @@
-vim.cmd.colorscheme('theme_hard')
+vim.cmd.colorscheme('theme_soft')
+
+vim.keymap.set({"", "!"}, '<M-1>', function() vim.cmd.colorscheme('theme_soft')   end, { noremap = true, silent = true })
+vim.keymap.set({"", "!"}, '<M-2>', function() vim.cmd.colorscheme('theme_medium') end, { noremap = true, silent = true })
+vim.keymap.set({"", "!"}, '<M-3>', function() vim.cmd.colorscheme('theme_hard')   end, { noremap = true, silent = true })
