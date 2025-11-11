1857b895a82a1194331586ecd11f4b71a8eba5b4
Michael Angelozzi <mangelozzi@gmail.com>
2025-11-11 11:31:55 +0200

    updated purge

--- .config/bash/ioec.sh
+++ .config/bash/ioec.sh
@@ -52,6 +52,6 @@ alias fe="cd $IOEC/gateway/Gateway/Gateway.Client; npm start"
 alias fen="cd $IOEC/gateway/Gateway/Gateway.NextClient; npm start"
 
 alias pg="cda; source ./activate.sh; echo 'clear; python run.py'"
-alias purgejs='cd ~/Ggateway/Gateway/Gateway.NextClient/src; find . -type f -name "*.js" -delete; cd ..';
+alias purgejs='cd ~/Ggateway/Gateway/Gateway.NextClient/src; find . -type f -name "*.js" ! -path "./public/link.js" -delete; cd ..'
 
 cdgn
